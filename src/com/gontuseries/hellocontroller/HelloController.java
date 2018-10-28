7b0f6deb1be3a3b7883aca4e53b86db4e13aecdc
farziengineer <anand.mohit001@gmail.com>
2018-10-28 14:07:09 +0530

    Added annotation based changes

--- src/com/gontuseries/hellocontroller/HelloController.java
+++ src/com/gontuseries/hellocontroller/HelloController.java
@@ -1,16 +1,15 @@
 package com.gontuseries.hellocontroller;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.AbstractController;
  
-public class HelloController extends AbstractController{
+@Controller
+public class HelloController {
  
-	@Override
-	protected ModelAndView handleRequestInternal(HttpServletRequest request,
-		HttpServletResponse response) throws Exception {
+	@RequestMapping("/")
+	public ModelAndView helloWorld(){
  
 		ModelAndView modelandview = new ModelAndView("HelloPage");
 		modelandview.addObject("welcomeMessage", "Hi User, welcome to the first Spring MVC Application");
