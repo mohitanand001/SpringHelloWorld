da1b81ceb2c63325d1fe456dfbc875f825e48ab9
farziengineer <anand.mohit001@gmail.com>
2018-10-28 14:18:15 +0530

    MultiActionController and RequestMapping at class level

--- src/com/gontuseries/hellocontroller/HelloController.java
+++ src/com/gontuseries/hellocontroller/HelloController.java
@@ -6,9 +6,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
  
 @Controller
+@RequestMapping("greet")
 public class HelloController {
  
-	@RequestMapping("/")
+	@RequestMapping("/welcome")
 	public ModelAndView helloWorld(){
  
 		ModelAndView modelandview = new ModelAndView("HelloPage");
@@ -16,4 +17,13 @@ public class HelloController {
 		
 		return modelandview;
 	}
+	
+	@RequestMapping("/hi")
+	public ModelAndView hiWorld(){
+		 
+		ModelAndView modelandview = new ModelAndView("HelloPage");
+		modelandview.addObject("welcomeMessage", "Hi User, welcome to the first Spring MVC Application");
+		
+		return modelandview;
+	}	
 }
